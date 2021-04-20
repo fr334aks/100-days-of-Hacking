36f1f247ccfeabf8712d31dc7b557bf54faba114
fraize <40591974+fraizerbrian@users.noreply.github.com>
2021-04-20 19:52:38 +0000

    Delete Fraize/6.OS_command_injection directory

--- Fraize/6.OS_command_injection/command_injector.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-domain_name = input("Enter the domain name: ")
-command = "nslookup {}".format(domain_name)
-response = subprocess.check_output(command, shell=True, encoding='UTF-8')
-print(response)
--- Fraize/6.OS_command_injection/command_injector_preventor.py
+++
@@ -1,9 +0,0 @@
-#preventing os injection using shlex
-import subprocess
-import shlex
-
-domain_name = domain("Enter the domain name: ")
-safe_domain_name =  shlex.quote(domain_name)
-command = "nslookup {}".format(safe_domain_name)
-response = subprocess.check_output(command, shell=True,encoding='UTF-8')
-print(response)
